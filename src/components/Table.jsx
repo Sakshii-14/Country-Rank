46ef0fb42de1996070016c22ae3636904a8ab502
Sakshii-14 <157134066+Sakshii-14@users.noreply.github.com>
2024-08-30 19:52:33 +0530

    Update Table.jsx

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import "../styles/container.css";
 
 function Table() {
   const list = useSelector((state) => state.filterslice.countryList);
