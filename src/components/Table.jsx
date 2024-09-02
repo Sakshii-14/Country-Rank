1afab06f4527edf3f9378d62b4b04991730d4ce2
sakshi kesharwani <sakshikesharwani14@gmail.com>
2024-09-02 20:39:54 +0530

    Cross browser compatibility

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -11,7 +11,7 @@ function Table() {
       <table className=" w-full sm:border-collapse border-separate border-spacing-3 ">
         <thead className=" border-b-2 border-[#282B30]">
           <tr className="h-[2.5rem] text-[#6C727F] text-[0.875rem]">
-            <th className=" align-top text-left sm:w-[10%] w-[40%] sm:border-b-0 border-b-2 border-[#282B30] emoji-text">
+            <th className=" align-top text-left sm:w-[10%] w-[40%] sm:border-b-0 border-b-2 border-[#282B30]">
               Flag
             </th>
             <th className=" text-left align-top sm:w-[15%] w-[40%] sm:border-b-0 border-b-2 border-[#282B30]">
@@ -36,7 +36,7 @@ function Table() {
                 key={list.cca3}
               >
               
-                  <td className="sm:text-[3.25rem] text-[2.5rem]">
+                  <td className="sm:text-[3.25rem] text-[2.5rem] emoji-text">
                   <Link to={`country/${list.name.common}`}>
                     {list.flag}
                     </Link>
